9d09c16d82d213932c14473752ca212414551f3d
Shubhanshu Kakkar <kakkarhimanshu889@gmail.com>
2023-07-27 17:51:28 +0530

    hamburger effect added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 const NavBar = () => {
-const [nav,setNav]=useState(false);
+  const [nav, setNav] = useState(false);
 
   const links = [
     {
       id: 1,
       link: "home",
     },
-    
+
     {
       id: 2,
       link: "about",
@@ -42,14 +42,27 @@ const [nav,setNav]=useState(false);
           </li>
         ))}
       </ul>
-      <div 
-      onClick={()=>setNav(!nav)}
-      
-      className="cursor-pinter pr-4 z-10 text-gray-500">
-
-        {nav ? <FaTimes size={30}/>:     
-        <FaBars size={30}/>}
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pinter pr-4 z-10 text-gray-500
+        md:hidden
+        "
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className=" px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
